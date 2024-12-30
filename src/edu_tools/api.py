4623b856e012173010c0ef8adc14fa8e364ae76c
pingfury <pingfury@outlook.com>
2024-12-31 01:00:09 +0800

    fix llms

--- src/edu_tools/api.py
+++ src/edu_tools/api.py
@@ -3,13 +3,28 @@ import os
 from dotenv import load_dotenv
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
-from edu_tools.llms.gemini import topic_formt as llm_topic_formt
+from edu_tools.llms.gemini import (
+    topic_formt as llm_topic_formt,
+    topic_answer as llm_topic_answer,
+    topic_analysis as llm_topic_analysis,
+)
 
 load_dotenv()
 
 app = FastAPI()
+
+origins = ["*"]  # 允许所有来源，仅限开发环境
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -24,5 +39,19 @@ class Topic(BaseModel):
 @app.post("/topic/formt")
 def topic_fromt(topic: Topic):
     text = llm_topic_formt(topic.text)
+    print(text)
+    return {"topic": text}
+
+
+@app.post("/topic/answer")
+def topic_answer(topic: Topic):
+    text = llm_topic_answer(topic.text)
+    print(text)
+    return {"topic": text}
+
+
+@app.post("/topic/analysis")
+def topic_answer(topic: Topic):
+    text = llm_topic_analysis(topic.text)
     print(text)
     return {"topic": text}
--- src/edu_tools/llms/gemini.py
+++ src/edu_tools/llms/gemini.py
@@ -15,6 +15,62 @@ def topic_formt(text: str):
     - 输出纯文本
     - 数学表达式在无法通过文本表达是,使用 Latex 公式代替
     - 不使用 Markdown 语法
+    - 只进行格式化
+    - 不计算,不补全
+    """
+    prompt_template = ChatPromptTemplate.from_messages(
+        [("system", system_template), ("user", "{text}")]
+    )
+    prompt = prompt_template.invoke({"text": text})
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+
+    response = model.invoke(prompt)
+    return response.content
+
+
+def topic_answer(text: str):
+    system_template = """
+    # Role 我是一个专门用于小学数学问答题解题的 AI 角色
+
+    ## Workflow
+    - 根据 input 描述的问答题目内容，给出题目的简短解题过程，和答案
+
+    ## Constrains
+    - 不使用 Markdown 语法
+    - 不要有“步骤” 之类的内容
+    - 不要有小学数学阶段以外的术语,概念
+    - 不要出现人称名称
+    - 以"答: '正确答案'。"结尾
+    - 标点符号使用中文符号
+    - 输出纯文本
+    - 数学表达式在无法通过文本表达是,使用 Latex 公式代替
+    """
+    prompt_template = ChatPromptTemplate.from_messages(
+        [("system", system_template), ("user", "{text}")]
+    )
+    prompt = prompt_template.invoke({"text": text})
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+
+    response = model.invoke(prompt)
+    return response.content
+
+
+def topic_analysis(text: str):
+    system_template = """
+    # Role 我是一个专门用于分析小学数学题解的解答 AI 角色
+
+    ## Workflow
+    - 根据输入描述的问答题目内容，给出正确的分析
+
+    ## Constrains
+    - 不使用 Markdown 语法
+    - 不要有“步骤” 之类的内容
+    - 不要有小学数学阶段以外的术语,概念
+    - 不要出现人称名称
+    - 以"故答案为: '正确答案'。"结尾
+    - 标点符号使用中文符号
+    - 输出纯文本
+    - 数学表达式在无法通过文本表达是,使用 Latex 公式代替
     """
     prompt_template = ChatPromptTemplate.from_messages(
         [("system", system_template), ("user", "{text}")]
