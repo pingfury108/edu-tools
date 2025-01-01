9bc70a1a8d73706fd39608929cf0b56c928263d5
pingfury <pingfury@outlook.com>
2025-01-02 03:53:58 +0800

    fix llms prompt

--- src/edu_tools/llms/gemini.py
+++ src/edu_tools/llms/gemini.py
@@ -2,6 +2,9 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 
 
+ModelName = "gemini-1.5-flash-latest"
+
+
 def topic_formt(text: str):
     system_template = """
     # Role 你是一个整理文本格式的AI角色
@@ -17,6 +20,8 @@ def topic_formt(text: str):
     - 句子需要通顺无歧义
     - 输出纯文本
     - 数学表达式在无法通过文本表达是,使用 Latex 公式代替
+    - 分数需要用 Latex 公式
+    - 数学表达式中有一个用 Latex 公式, 则整个表达式全部使用 Latext 公式, 例如 '0.72 × $\frac{{3}}{{8}}$ =' 要写为  '$0.72 × \frac{{3}}{{8}} =$'
     - 不使用 Markdown 语法
     - 只进行格式化
     - 不计算,不补全
@@ -31,7 +36,7 @@ def topic_formt(text: str):
         [("system", system_template), ("user", "{text}")]
     )
     prompt = prompt_template.invoke({"text": text})
-    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+    model = ChatGoogleGenerativeAI(model=ModelName)
 
     response = model.invoke(prompt)
     return response.content
@@ -42,23 +47,28 @@ def topic_answer(text: str):
     # Role 我是一个专门用于小学数学问答题解题的 AI 角色
 
     ## Workflow
-    - 根据 input 描述的问答题目内容，给出题目正确的简短解题过程，和答案
+    - 根据 input 描述的问答题目内容，给出题目简短计算过程，和正确答案, 不需要太多文字表述
 
     ## Constrains
     - 不使用 Markdown 语法
     - 不要有“步骤” 之类的内容
     - 不要有小学数学阶段以外的术语,概念
     - 不要出现人称名称
-    - 以"答: 答案。"结尾, "答案"题目的答案,并非 "答案"文字, 多个答案使,使用 '；'间隔
     - 标点符号使用中文符号
     - 输出纯文本
     - 数学表达式,使用 Latex 公式
+    - 分数需要用 Latex 公式
+    - 有多个小题时,分别给出解题过程和正确答案
+    - 不能有像: `7.8÷3.9×6.8 = 2×6.8 = 13.6` 这样的等式, 要这样的 `7.8÷3.9×6.8 \n= 2×6.8 \n= 13.6`, 可以有 `7.8÷3.9×6.8 = 13.6` 这样一个等号
+    - 余数使用 `……` 间隔
+    - 不能像有 `$0.72 \times \frac{{3}}{{8}} = 0.72 \times 0.375 = 0.27$ `这样的公式, 要这样的 `$0.72 \times \frac{{3}}{{8}}$\n$= 0.72 \times 0.375$\n $= 0.27$`
+
     """
     prompt_template = ChatPromptTemplate.from_messages(
         [("system", system_template), ("user", "{text}")]
     )
     prompt = prompt_template.invoke({"text": text})
-    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+    model = ChatGoogleGenerativeAI(model=ModelName)
 
     response = model.invoke(prompt)
     return response.content
@@ -81,13 +91,16 @@ def topic_analysis(text: str):
     - 以"故答案为: 答案。"结尾, "答案"题目的答案,并非 "答案"文字, 多个答案使,使用 '；'间隔
     - 标点符号使用中文符号
     - 输出纯文本
-    - 数学表达式,使用Latex 公式
+    - 分数需要用 Latex 公式
+    - 不能有像: `7.8÷3.9×6.8 = 2×6.8 = 13.6` 这样的等式, 要这样的 `7.8÷3.9×6.8 \n= 2×6.8 \n= 13.6`, 可以有 `7.8÷3.9×6.8 = 13.6` 这样一个等号
+    - 不能像有 `$0.72 \times \frac{{3}}{{8}} = 0.72 \times 0.375 = 0.27$ `这样的公式, 要这样的 `$0.72 \times \frac{{3}}{{8}}$\n$= 0.72 \times 0.375$\n $= 0.27$`
+    - 余数使用 `……` 间隔
     """
     prompt_template = ChatPromptTemplate.from_messages(
         [("system", system_template), ("user", "{text}")]
     )
     prompt = prompt_template.invoke({"text": text})
-    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+    model = ChatGoogleGenerativeAI(model=ModelName)
 
     response = model.invoke(prompt)
     return response.content
