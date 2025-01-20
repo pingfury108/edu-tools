1407cf8281809488735378436db575677d1e855d
pingfury <pingfury@outlook.com>
2025-01-21 02:49:07 +0800

    fix api

--- src/edu_tools/api.py
+++ src/edu_tools/api.py
@@ -72,9 +72,9 @@ def llm_run(item: str, ctx: LLMContext, req: Request):
         llm_fun = gemini_run
         if llm_provide == deepseek_provide:
             llm_fun = deepseek_run
-        log.debug(run_prompt)
-        text = llm_fun(run_prompt)
-        # log.info(deepseek_math_fromat(text))
+            log.debug(run_prompt)
+            text = llm_fun(run_prompt)
+            # log.info(deepseek_math_fromat(text))
         return {"topic": remove_empty_lines_from_string(text)}
     else:
         return {"msg": "Unsupported parameters"}
--- src/edu_tools/llms/context.py
+++ src/edu_tools/llms/context.py
@@ -4,9 +4,11 @@ from pydantic import BaseModel
 
 class LLMContext(BaseModel):
     topic: str
+    topic_type: Optional[str] = None
     image_url: Optional[str] = None
     answer: Optional[str] = None
     analysis: Optional[str] = None
+    image_data: Optional[str] = None
 
 
 class OCRContext(BaseModel):
